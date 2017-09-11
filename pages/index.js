d890503c2940e2ba175c7a2117df3451a2333ae8
Inumidun Amao <inumidunamao@Inumiduns-MacBook-Pro.local>
2017-09-11 06:54:33 +0100

    add connection to github

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import fetch from 'isomorphic-fetch';
+
+export default class Home extends React.Component {
+  state = {
+    username: null,
+    user: null
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { username } = this.state;
+    fetch(`https://api.github.com/users/${username}`)
+      .then(res => res.json())
+      .then(user => this.setState({ user }))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    const { user } = this.state;
+    return (
+      <div>
+        {user && (
+          <div>
+            <img src={user.avatar_url} alt={user.login} width={50} />
+            <h3>{user.name}</h3>
+            <p>{user.company}</p>
+          </div>
+        )}
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input
+            type="text"
+            placeholder="Type Github username"
+            onChange={e => this.setState({ username: e.target.value })}
+          />
+          <button type="submit">Go!</button>
+        </form>
+      </div>
+    );
+  }
+}
